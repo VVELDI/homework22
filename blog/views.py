1e03b183a7ac54c983570a0b483e51ddacdfcb98
Вилков Виталий <takao.nakamuro@yandex.ru>
2025-07-25 10:57:52 +0300

    Доработал приложение, добавил возможность редактирования блога. И создание блога через админ панель

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Blog
 from django.urls import reverse_lazy, reverse
-from django.shortcuts import redirect
+from .models import Blog
 
 class BlogListView(ListView):
     model = Blog
@@ -19,14 +18,14 @@ class BlogDetailView(DetailView):
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
         obj.views_count += 1
-        obj.save(update_fields=["views_count"])
+        obj.save(update_fields=['views_count'])
         return obj
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ['title', 'content', 'preview', 'is_published']
     template_name = 'blog/blog_form.html'
-    success_url = reverse_lazy('blog:blog_list')
+    success_url = reverse_lazy('blog:post_list')
 
 class BlogUpdateView(UpdateView):
     model = Blog
@@ -39,4 +38,4 @@ class BlogUpdateView(UpdateView):
 class BlogDeleteView(DeleteView):
     model = Blog
     template_name = 'blog/blog_confirm_delete.html'
-    success_url = reverse_lazy('blog:blog_list')
+    success_url = reverse_lazy('blog:post_list')
