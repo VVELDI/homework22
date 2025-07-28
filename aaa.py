f66d103e24cf427d596ff3e021fb0df9c7b4d139
Вилков Виталий <takao.nakamuro@yandex.ru>
2025-07-28 11:26:22 +0300

    дален файл с чувствительными данными

--- aaa.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-try:
-    server = smtplib.SMTP_SSL("smtp.yandex.ru", 465, timeout=10)
-    server.login("takao.nakamuro@yandex.ru", "qdeqwoninzkzdzim")
-    print("✅ Подключение успешно")
-    server.quit()
-except Exception as e:
-    print("❌ Ошибка:", e)
